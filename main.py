5fc78f292ff1371d3b9a663997bb2426924f96c2
RoiuGit <puzikov.nikita@gmail.com>
2022-12-18 17:30:22 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ def trigger_reader(d_t, d_n):
     l_b = 0
     r_b = 0
     if d_t in d_n:
-        text = d_n.split(d_t)[2]
+        text1 = d_n.split(d_t)[2]
+        text2 = d_n.split(d_t)[0]
         trigger = ''
-        for char in text:
+        for char in text1:
             if char == '{':
                 l_b += 1
             if char == '}':
@@ -17,36 +18,51 @@ def trigger_reader(d_t, d_n):
                     trigger = trigger + char
                 else:
                     trigger = trigger + char
-                    return trigger[1::]
                     break
-    else:
-        return ''
+        l_b = 0
+        c = 0
+        tag = ''
+        for char in text2[::-1]:
+            if char == '{':
+                l_b += 1
+            if l_b == 2:
+                c += 1
+                tag = tag + char
+                if c == 7:
+                    break
+        return trigger[1::], tag[-1:-4:-1]
 
 
 def static_template(s_t, d, c, c_l):
     if s_t[0] == '#':
-        comment = '\t' + s_t.replace('VFM ', '') + '\n'
+        comment = '\n\t' + s_t.replace('VFM ', '') + '\n'
         return comment
     else:
         loc_tag = 'RU_CL_' + d + '_' + s_t
         if loc_tag in c_l:
             template = '\t\ttext = {\n\t\t\ttrigger = {\n\t\t\t\texists = c:' + s_t + \
-                       '\n\t\t\t\tthis = c:' + s_t + '\n\t\t\t}\n\t\t\tlocalization_key = RU_CL_'\
-                       + d + '_' + s_t + ' ' + c[d] + '\n'
+                       '\n\t\t\t\tthis = c:' + s_t + '\n\t\t\t}\n\t\t\tlocalization_key = RU_CL_' \
+                       + d + '_' + s_t + ' ' + c[d] + '\n\t\t}\n'
             return template
 
 
 def dynamic_template(d_t, d, c, c_l, d_n):
     if d_t[0] == '#':
-        comment = '\t' + d_t.replace('VFM ', '') + '\n'
+        comment = '\n\t' + d_t.replace('VFM ', '') + '\n'
         return comment
     else:
         loc_tag = 'RU_CL_' + d + '_' + d_t
         if loc_tag in c_l:
-            trigger = trigger_reader(d_t, d_n).replace('\n','\n\t')
-            template = '\t\ttext = {\n\t\t\ttrigger = {\n\t\t\t\texists = c:' + \
-                       d_t + '\n\t\t\t\tthis = c:' + d_t + '\n' \
-                       + trigger + '\t\t\tlocalization_key = RU_CL_' + d + '_' + d_t + ' ' + c[d] + '\n'
+            try:
+                trigger = trigger_reader(d_t, d_n)[0].replace('\n', '\n\t')
+                tag = trigger_reader(d_t, d_n)[1]
+                template = '\t\ttext = {\n\t\t\ttrigger = {\n\t\t\t\texists = c:' + \
+                           tag + '\n\t\t\t\tthis = c:' + tag + '\n' \
+                           + trigger + '\n\t\t\tlocalization_key = RU_CL_' + d + '_' + d_t + ' ' + c[d] + '\n\t\t}\n'
+            except AttributeError:
+                template = ''
+            except TypeError:
+                template = ''
             return template
 
 
@@ -55,7 +71,7 @@ layout = [[sg.Text('Static Tag List'), sg.Input(), sg.FileBrowse(key='input_stat
           [sg.Text('Dynamic Tag List'), sg.Input(), sg.FileBrowse(key='input_dynamic_tags')],
           [sg.Text('Custom Tag Localization'), sg.Input(), sg.FileBrowse(key='custom_tag_loc_file')],
           [sg.Text('Dynamic Names File'), sg.Input(), sg.FileBrowse(key='dynamic_names_file')],
-          [sg.Text('Custom Localization'), sg. Input(), sg.FileBrowse(key='custom_loc_file')],
+          [sg.Text('Custom Localization'), sg.Input(), sg.FileBrowse(key='custom_loc_file')],
           [sg.Button('Submit')]]
 
 window = sg.Window('Vic3 Ru Loc Gen', layout)
@@ -88,7 +104,6 @@ try:
     custom_tag_loc = custom_tag_loc_file.read()
     custom_tag_loc_file.close()
 except FileNotFoundError:
-    print(f3_path)
     custom_tag_loc = ''
 try:
     dynamic_names_file = open(f4_path, 'r', encoding='utf-8')
@@ -118,12 +133,12 @@ comments = {'RP': "# genitive", "DP": "# dative", "VP": "# accusative", "TP": "#
             "PP": "# prepositional"}
 for declension in declensions:
     output = ''
-    for static_tag in static_tags:
-        if static_template(static_tag, declension, comments, custom_tag_loc) is not None:
-            output = output + static_template(static_tag, declension, comments, custom_tag_loc)
     for dynamic_tag in dynamic_tags:
         if dynamic_template(dynamic_tag, declension, comments, custom_tag_loc, dynamic_names) is not None:
             output = output + dynamic_template(dynamic_tag, declension, comments, custom_tag_loc, dynamic_names)
+    for static_tag in static_tags:
+        if static_template(static_tag, declension, comments, custom_tag_loc) is not None:
+            output = output + static_template(static_tag, declension, comments, custom_tag_loc)
     default_tag = '\n\ttext = {\n\t\tlocalization_key = RU_CL_' + declension + '_DEFAULT'
     custom_loc = custom_loc.replace(default_tag, output + default_tag)
 output_file.write(custom_loc)
